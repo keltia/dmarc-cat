15081d546cbf71f2c6da8d062918dd3e5e607b7b
Ollivier Robert <roberto@keltia.net>
2018-02-24 22:55:47 +0100

    Our driver.

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"os"
+	"log"
+	"flag"
+	"io/ioutil"
+)
+
+var (
+	// MyName is the application
+	MyName = "dmarc-cat"
+	// MyVersion is our version
+	MyVersion = "0.0.1"
+
+	fDebug   bool
+	fVerbose bool
+
+	tempdir  string
+)
+
+func init() {
+	flag.BoolVar(&fDebug, "D", false, "Debug mode")
+	flag.BoolVar(&fVerbose, "v", false, "Verbose mode")
+}
+
+// cleanupTemp removes the temporary directory
+func cleanupTemp(dir string) {
+	err := os.RemoveAll(dir)
+	if err != nil {
+		log.Printf("cleanup failed for %s: %v", dir, err)
+	}
+}
+
+// createSandbox creates our our directory with TEMPDIR (wherever it is)
+func createSandbox(tag string) (path string) {
+	// Extract in safe location
+	dir, err := ioutil.TempDir("", tag)
+	if err != nil {
+		log.Fatalf("unable to create sandbox %s: %v", dir, err)
+	}
+	return dir
+}
+
+
+func main() {
+	flag.Parse()
+
+	if fDebug {
+		fVerbose = true
+	}
+
+	if len(flag.Args()) != 1 {
+		log.Fatalln("You must specify at least one file.")
+	}
+
+	tempdir = createSandbox(MyName)
+	if err := handleSingleFile(tempdir, flag.Arg(0)); err != nil {
+		log.Printf("error parsing %s: %v", flag.Arg(0), err)
+	}
+	cleanupTemp(tempdir)
+}
