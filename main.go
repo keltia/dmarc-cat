e1918a7d672d3115112859e41e2fdc679380f91c
Ollivier Robert <roberto@keltia.net>
2018-05-08 11:22:41 +0200

    Welcome 0.3.0 with gzip support.

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ var (
 	// MyName is the application
 	MyName = filepath.Base(os.Args[0])
 	// MyVersion is our version
-	MyVersion = "0.2.1"
+	MyVersion = "0.3.0"
 	// Author should be abvious
 	Author = "Ollivier Robert"
 
