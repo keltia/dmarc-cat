0c980f87b612584e9b6195479097e0cafd8d124d
Ollivier Robert <roberto@keltia.net>
2018-10-06 12:30:20 +0200

    Begin tests for Analyze().

--- analyze_test.go
+++ analyze_test.go
@@ -1,10 +1,16 @@
 package main
 
 import (
-    "testing"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestAnalyze(t *testing.T) {
 
 }
 
+func TestGatherRows_Empty(t *testing.T) {
+	r := GatherRows(Feedback{})
+	assert.Empty(t, r)
+}
