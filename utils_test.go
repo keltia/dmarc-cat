917edb5e6260bab213d74ca5add1d615a9bf1e3e
Ollivier Robert <roberto@keltia.net>
2018-10-06 11:20:19 +0200

    Tests for verbose()/debug().

--- utils_test.go
+++ utils_test.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestVerbose_No(t *testing.T) {
+	verbose("no")
+}
+
+func TestVerbose_Yes(t *testing.T) {
+	fVerbose = true
+	verbose("yes")
+	fVerbose = false
+}
+
+func TestDebug_No(t *testing.T) {
+	debug("no")
+}
+
+func TestDebug_Yes(t *testing.T) {
+	fVerbose = true
+	verbose("yes")
+	fVerbose = false
+}
