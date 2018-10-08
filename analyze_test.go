4120246154506b6fb50a6a1a7d03020521273ba3
Ollivier Robert <roberto@keltia.net>
2018-10-08 21:08:03 +0200

    One more test for GatherRows().

--- analyze_test.go
+++ analyze_test.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/xml"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestAnalyze(t *testing.T) {
@@ -15,4 +17,22 @@ func TestAnalyze(t *testing.T) {
 func TestGatherRows_Empty(t *testing.T) {
 	r := GatherRows(Feedback{})
 	assert.Empty(t, r)
+}
+
+func TestGatherRows_Good(t *testing.T) {
+	file := "testdata/example.com!keltia.net!1538604008!1538690408.xml"
+
+	a, err := NewArchive(file)
+	require.NoError(t, err)
+
+	body, err := a.Extract(".xml")
+	require.NoError(t, err)
+
+	var report Feedback
+
+	err = xml.Unmarshal(body, &report)
+	require.NoError(t, err)
+
+	rows := GatherRows(report)
+	assert.Equal(t, 1, len(rows))
 }
