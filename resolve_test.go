0d15b43704137c0fdd776d4b5c47be2cfc8eaca3
Ollivier Robert <roberto@keltia.net>
2018-10-09 18:09:25 +0200

    Add tests for the Resolver interface.

--- resolve_test.go
+++ resolve_test.go
@@ -1,3 +1,23 @@
 package main
 
+import (
+	"testing"
 
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNullResolver_LookupAddr(t *testing.T) {
+	var r NullResolver
+
+	resp, err := r.LookupAddr("example.com")
+	assert.NoError(t, err)
+	assert.Equal(t, []string{"example.com"}, resp)
+}
+
+func TestRealResolver_LookupAddr(t *testing.T) {
+	var r RealResolver
+
+	resp, err := r.LookupAddr("8.8.8.8")
+	assert.NoError(t, err)
+	assert.Equal(t, []string{"google-public-dns-a.google.com."}, resp)
+}
