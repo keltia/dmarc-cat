a04a7d909bca5ac8ee620b52091b5fab9018ba6e
Ollivier Robert <roberto@keltia.net>
2018-10-10 10:52:32 +0200

    Add the tests.

--- main_test.go
+++ main_test.go
@@ -3,7 +3,28 @@ package main
 import (
 	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
+
+func TestSetup(t *testing.T) {
+	ctx := Setup([]string{})
+	assert.Nil(t, ctx)
+}
+
+func TestSetup2(t *testing.T) {
+	ctx := Setup([]string{"foo.zip"})
+	assert.NotNil(t, ctx)
+	assert.IsType(t, (*Context)(nil), ctx)
+}
+
+func TestSetup3(t *testing.T) {
+	fNoResolv = true
+	ctx := Setup([]string{"foo.zip"})
+	assert.NotNil(t, ctx)
+	assert.IsType(t, (*Context)(nil), ctx)
+	fNoResolv = false
+}
 
 // XXX I'm not sure how to really test main() — os.Args() is global and not reset between calls
 
