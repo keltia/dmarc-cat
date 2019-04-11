bef9ce3b73312c1d8503372d29ffef9f7b0d6164
Ollivier Robert <roberto@keltia.net>
2019-04-11 16:43:09 +0200

    WIP: accept file to be analysed through stdin.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
@@ -24,6 +25,7 @@ var (
 	fJobs     int
 	fNoResolv bool
 	fSort     string
+	fType     string
 	fVerbose  bool
 	fVersion  bool
 )
@@ -39,6 +41,7 @@ func init() {
 	flag.BoolVar(&fNoResolv, "N", false, "Do not resolve IPs")
 	flag.IntVar(&fJobs, "j", runtime.NumCPU(), "Parallel jobs")
 	flag.StringVar(&fSort, "S", `"Count" "dsc"`, "Sort results")
+	flag.StringVar(&fType, "t,type", "", "File type for stdin mode")
 	flag.BoolVar(&fVerbose, "v", false, "Verbose mode")
 	flag.BoolVar(&fVersion, "version", false, "Display version")
 }
@@ -74,6 +77,31 @@ func Setup(a []string) (*Context, error) {
 	return ctx, nil
 }
 
+func SelectInput(file string) (io.ReadCloser, error) {
+	debug("file=%s", file)
+	debug("file=%s", file)
+
+	if file == "-" {
+		if fType == "" {
+			return nil, errors.New("Wrong file type, use -t")
+		}
+		return os.Stdin, nil
+	}
+
+	// We have a filename
+	if !checkFilename(file) {
+		return nil, errors.New("bad filename")
+	}
+
+	// We want the full path
+	myfile, err := filepath.Abs(file)
+	if err != nil {
+		return nil, errors.Wrapf(err, "Abs(%s)", file)
+	}
+
+	return os.Open(myfile)
+}
+
 func realmain(args []string) error {
 	flag.Parse()
 
@@ -82,10 +110,33 @@ func realmain(args []string) error {
 		return errors.Wrap(err, "realmain")
 	}
 
+	var txt string
+
+	// Look for input file or stdin/"-"
 	file := args[0]
-	txt, err := HandleSingleFile(ctx, file)
-	if err != nil {
-		return errors.Wrapf(err, "file %s:", file)
+
+	verbose("Analyzing %s", file)
+
+	if filepath.Ext(file) == ".zip" ||
+		filepath.Ext(file) == ".ZIP" {
+
+		txt, err = HandleZipFile(ctx, file)
+		if err != nil {
+			return errors.Wrapf(err, "file %s:", file)
+		}
+	} else {
+		in, err := SelectInput(file)
+		if err != nil {
+			return errors.Wrap(err, "SelectInput")
+		}
+		defer in.Close()
+
+		typ := archive.Ext2Type(fType)
+
+		txt, err = HandleSingleFile(ctx, in, typ)
+		if err != nil {
+			return errors.Wrapf(err, "file %s:", file)
+		}
 	}
 	fmt.Println(txt)
 	return nil
