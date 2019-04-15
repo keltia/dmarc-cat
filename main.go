28764fc91d6c34ff4c2b8ce1e62dbe13f3edfa97
Ollivier Robert <roberto@keltia.net>
2019-04-15 16:30:59 +0200

    Welcome v0.10 with stdin support (from archive).

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ var (
 	// MyName is the application
 	MyName = filepath.Base(os.Args[0])
 	// MyVersion is our version
-	MyVersion = "0.9.2"
+	MyVersion = "0.10.0"
 	// Author should be abvious
 	Author = "Ollivier Robert"
 
